475d864d0e7f652cbff07d9a6f514e5e97203bda
Abraham White <abelincoln.white@gmail.com>
2018-08-07 16:16:11 -0600

    Add initial rules

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # tslint-rules
 tslint rules common to all shingo projects
+
+## Install
+```
+npm install --save-dev @shingo/tslint-config && npm install --save-dev tslint
+```
+
+## Usage
+Add the following to your `tslint.json`
+```json
+{
+  "extends": "@shingo/tslint-config"
+}
+```
+
---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+    extends: ['tslint:latest'],
+    rules: {
+        "arrow-parens": { options: ["ban-single-arg-parens"] },
+        "curly": { options: ["ignore-same-line"] },
+        "interface-name": { options: ["never-prefix"] },
+        // class members should be grouped by how they relate to each other not by access or type
+        "member-ordering": false,
+        // requiring members to be marked as public just adds noise
+        "member-access": { options: ["no-public"] },
+        "no-implicit-dependencies": { options: ["dev"] },
+        "no-unused-variable": { options: ["allow-leading-underscore"] },
+        // increases maintenance while providing little benefit
+        // (object keys are usally grouped by a different metric than lexical position)
+        "object-literal-sort-keys": false,
+        // Ordering imports provides little benefit while increasing maintenance
+        "ordered-imports": false,
+        "quotemark": { options: ["single"] },
+        // this is mostly stylistic, but js doesn't require parens and they create noise
+        "semicolon": { options: ["never"] },
+        "trailing-comma": {
+            options: {
+                "esSpecCompliant": true,
+                "multiline": {
+                    "objects": "always",
+                    "arrays": "always",
+                    "functions": "never"
+                },
+                "singleline": "never"
+            }
+        }
+        "variable-name": { options: ["allow-leading-underscore"] },
+    }
+}
